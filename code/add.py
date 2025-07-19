ad26939934ce42590431f8f3f439459d497bdd44
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-07-19 19:19:45 +0200

    feat: creating the function that handles the insertion of a new password
    It gets the to be added-password, it generates the master key (mk), it encrypts the password using the master key, and finally it adds the encrypted password to the database.

--- code/add.py
+++ code/add.py
@@ -12,4 +12,23 @@ def computeMasterKey(mp,ds):
   #using the PBDKF2 function from the Crypto.protocol module to derive a strong key from the two input values
   key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512) #(mp, ds, length_key, nrRoundsforAESstrengthening, hashFunctionSpecification)
   return key
-  
+
+def addEntry(mp, ds, sitename, siteurl, email, username): 
+  #get the password
+  password = getpass("Password: ")
+
+  mk = computeMasterKey(mp,ds): #mk stands for masterKey
+
+  #using imported aesutil function to encrypt the mk 
+  #this should return the encrypted password in base 64 encoded format
+  encrypted = utils.aesutil.encrypt(key=mk, source=password, keyType="bytes")
+
+  #add the new password to the database
+  db = dbconfig()
+  cursor = db.cursor()
+  query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
+  val = (sitename.siteurl,email.username.encrypted)
+  cursor.execute(query, val)
+  db.commit()
+
+printc("[green][+][/green] Added entry ")
