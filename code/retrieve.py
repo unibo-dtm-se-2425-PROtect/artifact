65704e780f9b51fd790bca4d32bb15581e31f239
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-07-21 20:21:27 +0200

    chore: import printc, Console, and Table
    Respectively from rich, rich.console, and rich.table to be used to display the queried results in a table-shaped format to the user.

--- code/retrieve.py
+++ code/retrieve.py
@@ -1,1 +1,6 @@
 from utils.dbconfig import dbconfig #needed to establish connection with the database
+
+#modules to be used in order to display the search results in a table-shaped format to the user
+from rich import print as printc
+from rich.console import Console
+from rich.table import Table
