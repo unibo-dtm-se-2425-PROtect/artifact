878cb8ae9ef63dec19ea476ce7abefc2806a2ad1
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-07-27 17:35:57 +0200

    fix: just deleting a useless space

--- code/generate.py
+++ code/generate.py
@@ -2,4 +2,4 @@ import random
 import string 
 
 def generatePassword(length):
-    return ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(length) ])
+    return ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(length)])
