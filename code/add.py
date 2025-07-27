27ee526edd03694c097a98c4319efc1d89ac46b1
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-07-27 16:20:51 +0200

    feat: import base64 to encode AES-256 encrypted data

--- code/add.py
+++ code/add.py
@@ -3,6 +3,7 @@ from dbconfig import dbconfig
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Random import get_random_bytes
+import base64
 
 import AES256util
 
