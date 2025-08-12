7ba363f4100a7672ab1a7196da4b9482c23c5003
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-12 12:14:31 +0200

    fix: error in defining the function into the variable

--- tests/test_dbconfig.py
+++ tests/test_dbconfig.py
@@ -7,7 +7,7 @@ def test_dbconfig_success(mock_connect):
     mock_db=MagicMock()
     mock_connect.return_value=mock_db
 
-    db=dbmod.dbconfig()
+    db=dbconfig()
 
     mock_connect.assert_called_once_with(
         host="localhost",
@@ -19,6 +19,6 @@ def test_dbconfig_success(mock_connect):
 @patch("project.dbconfig.mysql.connector.connect", side_effect=Exception("DB connection failed"))
 @patch("project.dbconfig.console.print_exception")
 def test_dbconfig_failure(mock_connect):
-    db=dbmod.dbconfig()
+    db=dbconfig()
     assert db is None #assuming the function returns None on failure
     mock_print_exception.assert_called_once()
