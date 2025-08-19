bd0866cceeed84f0af490e32317362ecc72e93df
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 17:30:25 +0200

    feat: initialization of the password manager model managing rows of tuples as entries which are strings

--- ui/MODEL/GUImodel.py
+++ ui/MODEL/GUImodel.py
@@ -1,4 +1,9 @@
 import random, string
 from typing import Tuple, List, Optional
 
-
+class PasswordManagerModel:
+    #Model to manage password entries (tuples)
+    def __init__(self):
+        self.entries:List[Tuple[str,str,str,str,str]]=[]
+    
+    
