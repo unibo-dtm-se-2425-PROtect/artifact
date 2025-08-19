7eb079742515af0a42fbad1cf83d28cfd429667a
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 12:39:28 +0200

    chore: import libraries (ttkbootstrap and tkinter to modify GUI graphically)

--- ui/VIEW/GUIview.py
+++ ui/VIEW/GUIview.py
@@ -1,1 +1,6 @@
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from tkinter import BOTH, X, Y, LEFT, RIGHT, END, messagebox, filedialog
+from typing import Callable, Optional, List, Tuple
+
 
