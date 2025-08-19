445c132e66717bb5ac9fb71f16de90e6b1fd22de
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 12:53:17 +0200

    feat(view): add "dumb" GUi interface letting the controller handle actions (on_sth will be called by the controller as commands, so we don't define their behavior inside the VIEW)

--- ui/VIEW/GUIview.py
+++ ui/VIEW/GUIview.py
@@ -3,4 +3,20 @@ from ttkbootstrap.constants import *
 from tkinter import BOTH, X, Y, LEFT, RIGHT, END, messagebox, filedialog
 from typing import Callable, Optional, List, Tuple
 
-
+class PasswordManagerView (tb.Frame):
+    #It only defines the GUI, doesn't consider db, crypto etc. The controller must be passed in to handle actions
+    def __init__(
+            self,
+            master,
+            on_add: callable[[], None],
+            on_edit: callable[[], None],
+            on_delete: callable[[], None],
+            on_copy: callable[[], None],
+            on_show: callable[[], None],
+            on_generate: callable[[], None],
+            on_export: callable[[], None],
+            on_import: callable[[], None],
+            on_lock: callable[[], None],
+            **kwargs
+    ):
+        super().__init__(master, padding=15, **kwargs)
