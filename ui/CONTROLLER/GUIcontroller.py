6afac7d92ee689c29ad09940ae28b3643c95836c
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 16:46:11 +0200

    feat: initialize PasswordManagerController to handle GUI logic

--- ui/CONTROLLER/GUIcontroller.py
+++ ui/CONTROLLER/GUIcontroller.py
@@ -1,4 +1,9 @@
 from ui.VIEW.GUIview import PasswordManagerView
 from typing import List, Tuple
 
+class PasswordManagerController:
+    def __init__(self, view:PasswordManagerView):
+        self.view=view
+
+        
 
