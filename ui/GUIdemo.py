558f8d5c730faaef12ed824c4f0325aaff17cbf8
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 13:38:49 +0200

    chore: add demo file to interactively test the GUI (not a test kind of file!)

---
+++ ui/GUIdemo.py
@@ -0,0 +1,4 @@
+#demo for GUI to create an interactive exploration for visual/manual verification 
+#It's not the TEST for GUI!
+#demo does not need to be tested, we directly test the MVC in the tests package 
+
