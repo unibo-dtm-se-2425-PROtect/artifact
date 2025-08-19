6e3cc49cfa53c8f5849c233daa32efeae2f3bb0a
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-08-19 16:49:36 +0200

    feat: bind view callbacks to controller methods

--- ui/CONTROLLER/GUIcontroller.py
+++ ui/CONTROLLER/GUIcontroller.py
@@ -5,5 +5,17 @@ class PasswordManagerController:
     def __init__(self, view:PasswordManagerView):
         self.view=view
 
+        #Bind view callbacks to controller methods
+        self.view.on_add=self.add
+        self.view.on_edit=self.edit
+        self.view.on_delete=self.delete
+        self.view.on_copy=self.copy
+        self.view.on_generate=self.generate
+        self.view.on_show=self.show
+        self.view.on_export=self.export
+        self.view.on_import=self.import_
+        self.view.lock=self.lock
+
         
+
 
