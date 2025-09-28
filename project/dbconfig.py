5a7e48553fb45e31d25f454264dd9d59fe2e8ccc
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-09-28 17:08:17 +0200

    style(CLI): improve output formatting with printc

--- project/dbconfig.py
+++ project/dbconfig.py
@@ -10,8 +10,10 @@ def dbconfig():
           host="localhost",
           user="pm",
           password="password"
-          )
+        )
+        printc("[green][+][/green] Connected to db")
     except Exception as e:
+        printc("[red][!] An error occurred while trying to connect to the database [/red]")
         console.print_exception(show_locals=True)
 
     return db
