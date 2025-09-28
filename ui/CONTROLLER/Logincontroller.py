2e917af9156dc27a94b072e5bafde3e3cfea3077
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-09-28 18:23:23 +0200

    chore: add imports

---
+++ ui/CONTROLLER/Logincontroller.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox
+from ui.VIEW.Loginview import LoginView
+from ui.MODEL.GUImodel import PasswordManagerModel
+from project.add import computeMasterKey
+import os
+
