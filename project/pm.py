6b16937faa4aebc090032c62c9445d0620531125
Francesca Gaeta <francesca.gaeta2@studio.unibo.it>
2025-10-12 19:50:56 +0200

    fix: restore parser command for help to the user about length of password

--- project/pm.py
+++ project/pm.py
@@ -19,6 +19,7 @@ parser.add_argument("-s", "--name", help="Site name")
 parser.add_argument("-u", "--url", help="Site URL")
 parser.add_argument("-e", "--email", help="Email")
 parser.add_argument("-l", "--login", help="Username")
+parser.add_argument("--length", help="Length of the password to generate",type=int)
 parser.add_argument("-c", "--copy", action='store_true', help='Copy password to clipboard')
 parser.add_argument("--all", action="store_true", help="Retrieve all stored entries (not the default, it must be specified for conscious choice)" )
 
