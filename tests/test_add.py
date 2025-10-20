e82e92ec90a78c01725453cb45ac4aceda8289cb
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-10-20 16:16:34 +0200

    test: Add test for fast_computeMasterKey function
    Add test to verify return type and length of fast_computeMasterKey output

--- tests/test_add.py
+++ tests/test_add.py
@@ -10,3 +10,10 @@ from Crypto.Hash import SHA512
 def fast_computeMasterKey(mp: str,ds: str) -> bytes:
     return PBKDF2(mp.encode(), ds.encode(), 32, count=100, hmac_hash_module=SHA512)
   
+#verifying that the output is of type bytes and length 32
+def test_computeMasterKey_return_type_and_length():
+    mp = "masterPassword"
+    ds = "deviceSecret"
+    k = fast_computeMasterKey(mp, ds)
+    assert isinstance(k, (bytes,bytearray))
+    assert len(k) == 32
