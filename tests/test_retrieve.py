2f7498e3c6ec2655b42928ea5411ed6ed820c641
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-10-23 18:24:46 +0200

    test: add imports for pytest and mocking in tests
    Added necessary imports for testing purposes.

--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -1,1 +1,4 @@
-
+import pytest
+from types import SimpleNamespace #it is used to create a fake lightweight DB object
+from unittest.mock import patch, MagicMock
+import project.retrieve as retrieve
