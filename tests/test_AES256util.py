fb84748095ca935ce0983627f2906c0a87e1aff9
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-12-06 20:25:45 +0100

    test: implementing FakeCursor class in test_AES256util.py
    This will simulate cursor module provided by dbconfig.py

--- tests/test_AES256util.py
+++ tests/test_AES256util.py
@@ -4,3 +4,17 @@ import types #used to create new module objects at runtime
 import base64 #used to encode and decode binary ciphertext to/from a printable ASCII form 
 import hashlib #used to compute cryptographic hash digests (SHA-256) using the Python standard library
 import pytest 
+
+
+class _FakeCursor:
+    def __init__(self, result):
+        self._result = result
+        self.executed = None
+        self.params = None
+
+    def execute(self, query, params=None):
+        self.executed = query
+        self.params = params
+
+    def fetchone(self):
+        return self._result
