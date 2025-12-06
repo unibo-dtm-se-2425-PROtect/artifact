a6e2022525f4802b6604b11610429c1ddc4dda8f
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2025-12-06 20:23:56 +0100

    test: add imports needed for test_AES256util.py

--- tests/test_AES256util.py
+++ tests/test_AES256util.py
@@ -1,1 +1,6 @@
-
+import sys 
+import importlib #used to import or reload modules programmatically
+import types #used to create new module objects at runtime
+import base64 #used to encode and decode binary ciphertext to/from a printable ASCII form 
+import hashlib #used to compute cryptographic hash digests (SHA-256) using the Python standard library
+import pytest 
