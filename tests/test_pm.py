707e43b746dad303ac179a0f3f8dde50a6d13493
Filippo7206 <filippo.bonaldo@studio.unibo.it>
2026-01-13 10:47:11 +0100

    test: create mock_db_setup for mocking the database connection for password validation

--- tests/test_pm.py
+++ tests/test_pm.py
@@ -3,3 +3,16 @@ from unittest.mock import patch, MagicMock
 import sys
 import hashlib
 import project.pm as pm
+
+
+#pytest fixtures
+
+@pytest.fixture
+def mock_db_setup():
+    #Mocking the database connection for password validation
+    with patch("project.pm.dbconfig") as mock_conn:
+        mock_db = MagicMock()
+        mock_cursor = MagicMock()
+        mock_conn.return_value = mock_db
+        mock_db.cursor.return_value = mock_cursor
+        yield mock_cursor
